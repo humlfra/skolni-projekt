8005d27960672d515093422603972dc8fdfdcfd5
humlfra <humlfra@vosis.cz>
2022-05-10 02:45:26 +0200

    Update tabulka_navstevy.blade.php

--- resources/views/tabulka_navstevy.blade.php
+++ resources/views/tabulka_navstevy.blade.php
@@ -15,6 +15,7 @@
                     </tr>
                     @foreach ($users as $user)
                     <tr>
+                        
                         <td>{{ $user->id }}</td>
                         <td>{{ $user->name }}</td>
                     </tr>
